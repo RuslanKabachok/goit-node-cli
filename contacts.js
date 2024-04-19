62c4c65bb8254335a3762ff2f610317283939803
RuslanKabachok <ruslan.kabachok@gmail.com>
2024-04-19 21:53:11 +0300

    finished

--- contacts.js
+++ contacts.js
@@ -1,1 +1,56 @@
-const fs = require('node:fs/promises');
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import crypto from 'node:crypto';
+
+const contactsPath = path.resolve('db', 'contacts.json');
+
+async function listContacts() {
+  const contacts = await fs.readFile(contactsPath, {
+    encoding: 'utf-8',
+  });
+  return JSON.parse(contacts);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+
+  if (typeof contact === 'undefined') {
+    return null;
+  }
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((book) => book.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+  const removedContact = contacts[index];
+  contacts.splice(index, 1);
+
+  return removedContact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    name,
+    email,
+    phone,
+    id: crypto.randomUUID(),
+  };
+
+  contacts.push(newContact);
+
+  return newContact;
+}
+
+export default {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-import { program } from "commander";
+import { program } from 'commander';
+import Contacts from './contacts.js';
+
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
 program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case "list":
-      // ...
+    case 'list':
+      const contacts = await Contacts.listContacts();
+      console.log(contacts);
       break;
 
-    case "get":
-      // ... id
+    case 'get':
+      const contact = await Contacts.getContactById(id);
+      console.log(contact);
       break;
 
-    case "add":
-      // ... name email phone
+    case 'add':
+      const newContact = await Contacts.addContact(name, email, phone);
+      console.log(newContact);
       break;
 
-    case "remove":
-      // ... id
+    case 'remove':
+      const removedContact = await Contacts.removeContact(id);
+      console.log(removedContact);
       break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('\x1B[31m Unknown action type!');
   }
 }
 
